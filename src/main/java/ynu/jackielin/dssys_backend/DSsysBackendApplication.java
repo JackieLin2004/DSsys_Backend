51139ce4dce595871a8b9df4b3fb20026e221f49
JackieLin2004 <linxy20040304@163.com>
2024-11-19 15:15:23 +0800

    First commit

---
+++ src/main/java/ynu/jackielin/dssys_backend/DSsysBackendApplication.java
@@ -0,0 +1,13 @@
+package ynu.jackielin.dssys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSsysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DSsysBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ynu/jackielin/dssys_backend/DSsysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.jackielin.dssys_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSsysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
