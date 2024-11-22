47e9f04b91a8da9b8b0542a716da704ef097bb68
JackieLin2004 <linxy20040304@163.com>
2024-11-22 20:15:57 +0800

    Complete jwt request header validation

--- src/main/java/ynu/jackielin/dssys_backend/config/SecurityConfiguration.java
+++ src/main/java/ynu/jackielin/dssys_backend/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,8 +14,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ynu.jackielin.dssys_backend.dto.response.AuthorizeVO;
 import ynu.jackielin.dssys_backend.entity.RestBean;
+import ynu.jackielin.dssys_backend.filter.JwtAuthorizeFilter;
 import ynu.jackielin.dssys_backend.utils.JwtUtils;
 
 import java.io.IOException;
@@ -27,6 +30,9 @@ public class SecurityConfiguration {
 
     @Resource
     JwtUtils jwtUtils;
+
+    @Resource
+    JwtAuthorizeFilter jwtAuthorizeFilter;
 
     /**
      * 配置安全过滤链，定义了接口的权限控制规则、登录、登出逻辑以及会话管理策略
@@ -56,11 +62,18 @@ public class SecurityConfiguration {
                         .logoutUrl("/auth/logout") // 登出接口路径
                         .logoutSuccessHandler(this::onLogoutSuccess) // 登出成功处理
                 )
+                // 配置异常处理
+                .exceptionHandling(conf -> conf
+                        .authenticationEntryPoint(this::onUnauthorized)
+                        .accessDeniedHandler(this::onAccessDeny)
+                )
                 // 禁用 CSRF
                 .csrf(AbstractHttpConfigurer::disable)
                 // 配置无状态会话管理策略
                 .sessionManagement(conf -> conf
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 添加 JWT 授权过滤器
+                .addFilterBefore(jwtAuthorizeFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -89,6 +102,20 @@ public class SecurityConfiguration {
     }
 
     /**
+     * 登出成功后的处理方法
+     *
+     * @param request        HttpServletRequest 对象
+     * @param response       HttpServletResponse 对象
+     * @param authentication Authentication 对象，包含用户的认证信息
+     * @throws IOException   如果 IO 出现异常
+     * @throws ServletException 如果 Servlet 出现异常
+     */
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Authentication authentication) throws IOException, ServletException {
+    }
+
+    /**
      * 登录失败后的处理方法
      *
      * @param request   HttpServletRequest 对象
@@ -102,20 +129,39 @@ public class SecurityConfiguration {
                                         AuthenticationException exception) throws IOException, ServletException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(RestBean.failure(401, exception.getMessage()).asJsonString());
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
 
     /**
-     * 登出成功后的处理方法
+     * 权限不足时的处理方法
      *
-     * @param request        HttpServletRequest 对象
-     * @param response       HttpServletResponse 对象
-     * @param authentication Authentication 对象，包含用户的认证信息
-     * @throws IOException   如果 IO 出现异常
+     * @param request   HttpServletRequest 对象
+     * @param response  HttpServletResponse 对象
+     * @param exception AccessDeniedException 异常，包含权限不足的具体原因
+     * @throws IOException 如果 IO 出现异常
      * @throws ServletException 如果 Servlet 出现异常
      */
-    public void onLogoutSuccess(HttpServletRequest request,
-                                HttpServletResponse response,
-                                Authentication authentication) throws IOException, ServletException {
+    public void onAccessDeny(HttpServletRequest request,
+                             HttpServletResponse response,
+                             AccessDeniedException exception) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(RestBean.forbidden(exception.getMessage()).asJsonString());
+    }
+
+    /**
+     * 未认证时的处理方法
+     *
+     * @param request   HttpServletRequest 对象
+     * @param response  HttpServletResponse 对象
+     * @param exception AuthenticationException 异常，包含未认证的具体原因
+     * @throws IOException 如果 IO 出现异常
+     */
+    public void onUnauthorized(HttpServletRequest request,
+                               HttpServletResponse response,
+                               AuthenticationException exception) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(RestBean.unauthorized(exception.getMessage()).asJsonString());
     }
 }
--- src/main/java/ynu/jackielin/dssys_backend/utils/JwtUtils.java
+++ src/main/java/ynu/jackielin/dssys_backend/utils/JwtUtils.java
@@ -1,14 +1,20 @@
 package ynu.jackielin.dssys_backend.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtils {
@@ -18,6 +24,26 @@ public class JwtUtils {
 
     @Value("${spring.security.jwt.expire}")
     int expire;
+
+    /**
+     * 解析并验证 JWT Token
+     *
+     * @param headerToken 带有 "Bearer " 前缀的 Token 字符串
+     * @return 如果验证成功，返回解析后的 DecodedJWT 对象；如果验证失败或过期，返回 null
+     */
+    public DecodedJWT resolveJwt(String headerToken) {
+        String token = this.convertToken(headerToken);
+        if (token == null || token.isEmpty()) return null;
+        Algorithm algorithm = Algorithm.HMAC256(key);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+            DecodedJWT verify = jwtVerifier.verify(token);
+            Date expiresAt = verify.getExpiresAt();
+            return new Date().after(expiresAt) ? null : verify;
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
 
     /**
      * 创建 JWT Token
@@ -32,7 +58,7 @@ public class JwtUtils {
         Date expire = this.expireTime();
         return JWT.create()
                 .withClaim("id", id)
-                .withClaim("name", username)
+                .withClaim("username", username)
                 .withClaim("authorities", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
                 .withExpiresAt(expire)
                 .withIssuedAt(new Date())
@@ -49,5 +75,43 @@ public class JwtUtils {
         // 在当前时间基础上增加过期天数（单位：小时）
         calendar.add(Calendar.HOUR, expire * 24);
         return calendar.getTime();
+    }
+
+    /**
+     * 将解析后的 JWT 转换为 UserDetails 对象
+     *
+     * @param jwt DecodedJWT 对象，包含已解析的 Token 信息
+     * @return 用户详情对象，包含用户名、权限等信息
+     */
+    public UserDetails toUser(DecodedJWT jwt) {
+        Map<String, Claim> claims = jwt.getClaims();
+        return User
+                .withUsername(claims.get("username").asString())
+                .password("******")
+                .authorities(claims.get("authorities").asArray(String.class))
+                .build();
+    }
+
+    /**
+     * 从解析后的 JWT 中提取用户 ID
+     *
+     * @param jwt DecodedJWT 对象，包含已解析的 Token 信息
+     * @return 用户 ID
+     */
+    public Integer toId(DecodedJWT jwt) {
+        Map<String, Claim> claims = jwt.getClaims();
+        return claims.get("id").asInt();
+    }
+
+    /**
+     * 转换带有 "Bearer " 前缀的 Token 字符串为实际的 Token
+     *
+     * @param headerToken 带有 "Bearer " 前缀的 Token 字符串
+     * @return 去除前缀后的 Token 字符串；如果格式错误，返回 null
+     */
+    public String convertToken(String headerToken) {
+        if (headerToken == null || !headerToken.startsWith("Bearer "))
+            return null;
+        return headerToken.substring(7);
     }
 }
